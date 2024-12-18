ba6b5b169ec1205dec448465b1aa6985bb69e3ac
Malak-Amr-Elsadek <malakelsadek.2015@gmail.com>
2024-12-18 15:02:55 +0300

    I geuss

---
+++ main.py
@@ -0,0 +1,7 @@
+def I_geuss(name,colour):
+    print ("I will geuss your name is ",name)
+    print ("I will geuss your favourite colour is ",colour)
+
+name=input("Enter name:") 
+colour=input("Enter favourite colour:")
+I_geuss(name,colour) 
